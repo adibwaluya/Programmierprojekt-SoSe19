736914433b1e5820c4c9619e79c8faeb44c98dc3
Maximilian <s0564319@htw-berlin.de>
2019-05-09 22:46:37 +0200

    bla

---
+++ PP-STL Datei Oeffner/ErrorHandling/IErrorHandling.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorHandling
+{
+    interface IErrorHandling
+    {
+        void FindError();
+        void repair();
+    }
+}
