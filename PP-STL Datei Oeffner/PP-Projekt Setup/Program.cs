8cfc936d58a48a3e86ac6ff76a43786e292bb2ce
Michael Reno <s0565907@htw-berlin.de>
2019-05-09 19:20:00 +0200

    Projekt-Setup Test #1; New VS17 Projectmappe and Project with 1 Interface IStlSpeichern and 1 Class StlSpeichern.

---
+++ PP-STL Datei Oeffner/PP-Projekt Setup/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PP_Projekt_Setup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StlSpeichern Save = new StlSpeichern();
+            Save.StlDataName("Meine geile STL Datei");
+
+        }
+    }
+}
---
+++ PP-STL Datei Oeffner/PP-Projekt Setup/StlSpeichern.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Projekt_Setup
+{
+    class StlSpeichern: IStlSpeichern
+    {
+        private string DataName;
+        private int Type;
+        private string Locate;
+
+        public void StlDataName(string NewName)
+        {
+            DataName = NewName;
+        }
+
+        public void StlDataType(int TheType)
+        {
+            Type = TheType;
+        }
+
+        public void StlSaveLocation(string NewLocation)
+        {
+            Locate = NewLocation;
+        }
+    }
+}
