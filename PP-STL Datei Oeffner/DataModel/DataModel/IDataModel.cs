daed6f188e3b3295569a8665eb98cc83b64557c1
Adib Ghassani Waluya <adibwaluya44@gmail.com>
2019-05-10 18:09:49 +0200

    Added DataModel

---
+++ PP-STL Datei Oeffner/DataModel/DataModel/IDataModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    interface IDataModel
+    {
+        Object getPoints();
+        Object getEdges();
+        Object getSurfaces();
+        Object getBodies();
+
+        Object setPoint();
+        Object setEdge();
+        Object setSurface();
+
+        Object addPoint();
+        Object addEdge();
+        Object addSurface();
+    }
+}
