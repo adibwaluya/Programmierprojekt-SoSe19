524dd96a8d3a77c5072fb6609e989ba18f60d1ce
Michael Kaip <michael.kaip@student.htw-berlin.de>
2019-05-11 11:43:36 +0200

    Interface IDatamodel implemented in ViewModel.

---
+++ PP-STL Datei Oeffner/ViewModel/ViewModelTestClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataModel;
+
+
+namespace ViewModel
+{
+    class ViewModelTestClass : IDataModel
+    {
+        public object addEdge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object addPoint()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object addSurface()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object getBodies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object getEdges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object getPoints()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object getSurfaces()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object setEdge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object setPoint()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object setSurface()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
