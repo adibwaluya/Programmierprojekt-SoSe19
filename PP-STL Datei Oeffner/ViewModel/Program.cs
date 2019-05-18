a508cc8eb8e9a0b8f334812d7112d246a2eb1809
Michael Kaip <michael.kaip@student.htw-berlin.de>
2019-05-18 03:10:53 +0200

    Some changes to ViewModel applied

--- PP-STL Datei Oeffner/ViewModel/Program.cs
+++ PP-STL Datei Oeffner/ViewModel/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using OpenTK;
 
 namespace ViewModel
@@ -12,8 +9,7 @@ namespace ViewModel
         static void Main(string[] args)
         {
             GameWindow window = new GameWindow(500,500);
-
-            ViewModelWindow viewModelWindow = new ViewModelWindow(window);
+            ViewModelWindow modelWindow = new ViewModelWindow(window);
         }
     }
 }
--- PP-STL Datei Oeffner/ViewModel/ViewModelWindow.cs
+++ PP-STL Datei Oeffner/ViewModel/ViewModelWindow.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
 
@@ -13,32 +14,106 @@ namespace ViewModel
             Start();
         }
 
-        void Start()
+        private void Start()
         {
             window.Load += Loaded;
-            window.Resize += ResizeWindow;
             window.RenderFrame += RenderF;
-            window.Run(1.0 / 60.0);
+            window.Resize += ResizeWindow;
+            window.Run(1.0/60); // Frequency
         }
 
-        void Loaded(object o, EventArgs e)
-        {
-            GL.ClearColor(0.0f,0.0f,0.0f,0.0f);
-        }
-
-        void ResizeWindow(object o, EventArgs e)
+        /*
+         * Initializing the Viewport - the area in which objects are going to be rendered inside the window.
+         * Multiple viewports are poosible!!!
+         */
+        private void ResizeWindow(object sender, EventArgs e)
         {
             GL.Viewport(0, 0, window.Width, window.Height);
-
+            GL.MatrixMode(MatrixMode.Projection); //Initializes the projection matrix.
+            GL.LoadIdentity(); // Resets the state of the current matrix.
+            GL.Ortho(-50, 50.0, -50.0, 50.0, -10.0, 10.0); //Orthographic projection.
+            GL.MatrixMode(MatrixMode.Modelview);
         }
 
-        void RenderF(object o, EventArgs e)
+        private void RenderF(object sender, FrameEventArgs e)
         {
             GL.Clear(ClearBufferMask.ColorBufferBit);
-            /*
-             * Any Function, which ist going to be called from here that draws something on the screen will take affect.
-             */
+
+
+            //Any function which draws something on the screen has to be called between "Clear()" and "SwapBuffers()"
+
+            //GL.Begin(mode: BeginMode.Triangles); //"Triangles" ist just one of different available primitives.
+            //setColor();
+            //GL.Vertex2(1.0, 1.0);
+            //GL.Vertex2(49.0, 1.0);
+            //GL.Vertex2(25.0, 49.0);
+            //GL.End();
+
+            GL.Rotate();
+
+            GL.Begin(mode: BeginMode.Quads);
+            // front
+            //GL.Color3(1.0, 1.0, 0.0);
+            GL.Vertex3(-10.0, 10.0, 10.0);
+            GL.Vertex3(-10.0, 10.0, -10.0);
+            GL.Vertex3(-10.0, -10.0, -10.0);
+            GL.Vertex3(-10.0, -10.0, 10.0);
+
+            // back
+            //GL.Color3(1.0, 0.0, 1.0);
+            GL.Vertex3(10.0, 10.0, 10.0);
+            GL.Vertex3(10.0, 10.0, -10.0);
+            GL.Vertex3(10.0, -10.0, -10.0);
+            GL.Vertex3(10.0, -10.0, 10.0);
+
+            // top
+            //GL.Color3(0.0, 1.0, 1.0);
+            GL.Vertex3(10.0, -10.0, 10.0);
+            GL.Vertex3(10.0, -10.0, -10.0);
+            GL.Vertex3(-10.0, -10.0, -10.0);
+            GL.Vertex3(-10.0, -10.0, 10.0);
+
+            // bottom
+            //GL.Color3(1.0, 0.0, 0.0);
+            GL.Vertex3(10.0, 10.0, 10.0);
+            GL.Vertex3(10.0, 10.0, -10.0);
+            GL.Vertex3(-10.0, 10.0, -10.0);
+            GL.Vertex3(-10.0, 10.0, 10.0);
+
+            // right
+            //GL.Color3(0.0, 1.0, 0.0);
+            GL.Vertex3(10.0, 10.0, -10.0);
+            GL.Vertex3(10.0, -10.0, -10.0);
+            GL.Vertex3(-10.0, -10.0, -10.0);
+            GL.Vertex3(-10.0, 10.0, -10.0);
+
+            // left
+            //GL.Color3(0.0, 0.0, 1.0);
+            GL.Vertex3(10.0, 10.0, 10.0);
+            GL.Vertex3(10.0, -10.0, 10.0);
+            GL.Vertex3(-10.0, -10.0, 10.0);
+            GL.Vertex3(-10.0, 10.0, 10.0);
+
+            GL.End();
+
             window.SwapBuffers();
+        }
+
+        /*
+         * Checks if user settings according to the color are available - otherwise sets the color to default.
+         */
+        private void setColor()
+        {
+            GL.Color3(0.151, 0.255, 0.255); // default color values
+        }
+
+        /*
+         * Clearing the color of the frame and setting it to its default value
+         */
+        private void Loaded(object sender, EventArgs e)
+        {
+            GL.ClearColor(0f, 0f, 0f, 0f);
+
         }
     }
 }
