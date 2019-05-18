98233da16722dfa8d5e8cc4c8d230f9dce9f5c08
Michael Kaip <michael.kaip@student.htw-berlin.de>
2019-05-18 14:48:27 +0200

    Perspective projection for rendering 3D-Models implemented

--- PP-STL Datei Oeffner/ViewModel/ViewModelWindow.cs
+++ PP-STL Datei Oeffner/ViewModel/ViewModelWindow.cs
@@ -8,6 +8,8 @@ namespace ViewModel
     class ViewModelWindow
     {
         private readonly GameWindow window;
+        private float ProjectionAngle = 45.0f; // Angle for perspective projection mode
+
         public ViewModelWindow(GameWindow window)
         {
             this.window = window;
@@ -31,70 +33,22 @@ namespace ViewModel
             GL.Viewport(0, 0, window.Width, window.Height);
             GL.MatrixMode(MatrixMode.Projection); //Initializes the projection matrix.
             GL.LoadIdentity(); // Resets the state of the current matrix.
-            GL.Ortho(-50, 50.0, -50.0, 50.0, -10.0, 10.0); //Orthographic projection.
+
+            // Setup for the perspective projection (3D) (another possibility, orthographic projection (2D), is also available: GL.Ortho();)
+            Matrix4 matrix = Matrix4.CreatePerspectiveFieldOfView(ProjectionAngle * (float)Math.PI / 180, 
+                window.Width/window.Height, 1.0f, 100.0f);
+            GL.LoadMatrix(ref matrix);
+
             GL.MatrixMode(MatrixMode.Modelview);
         }
-
+         
         private void RenderF(object sender, FrameEventArgs e)
         {
+            GL.LoadIdentity();
             GL.Clear(ClearBufferMask.ColorBufferBit);
-
 
             //Any function which draws something on the screen has to be called between "Clear()" and "SwapBuffers()"
 
-            //GL.Begin(mode: BeginMode.Triangles); //"Triangles" ist just one of different available primitives.
-            //setColor();
-            //GL.Vertex2(1.0, 1.0);
-            //GL.Vertex2(49.0, 1.0);
-            //GL.Vertex2(25.0, 49.0);
-            //GL.End();
-
-            GL.Rotate();
-
-            GL.Begin(mode: BeginMode.Quads);
-            // front
-            //GL.Color3(1.0, 1.0, 0.0);
-            GL.Vertex3(-10.0, 10.0, 10.0);
-            GL.Vertex3(-10.0, 10.0, -10.0);
-            GL.Vertex3(-10.0, -10.0, -10.0);
-            GL.Vertex3(-10.0, -10.0, 10.0);
-
-            // back
-            //GL.Color3(1.0, 0.0, 1.0);
-            GL.Vertex3(10.0, 10.0, 10.0);
-            GL.Vertex3(10.0, 10.0, -10.0);
-            GL.Vertex3(10.0, -10.0, -10.0);
-            GL.Vertex3(10.0, -10.0, 10.0);
-
-            // top
-            //GL.Color3(0.0, 1.0, 1.0);
-            GL.Vertex3(10.0, -10.0, 10.0);
-            GL.Vertex3(10.0, -10.0, -10.0);
-            GL.Vertex3(-10.0, -10.0, -10.0);
-            GL.Vertex3(-10.0, -10.0, 10.0);
-
-            // bottom
-            //GL.Color3(1.0, 0.0, 0.0);
-            GL.Vertex3(10.0, 10.0, 10.0);
-            GL.Vertex3(10.0, 10.0, -10.0);
-            GL.Vertex3(-10.0, 10.0, -10.0);
-            GL.Vertex3(-10.0, 10.0, 10.0);
-
-            // right
-            //GL.Color3(0.0, 1.0, 0.0);
-            GL.Vertex3(10.0, 10.0, -10.0);
-            GL.Vertex3(10.0, -10.0, -10.0);
-            GL.Vertex3(-10.0, -10.0, -10.0);
-            GL.Vertex3(-10.0, 10.0, -10.0);
-
-            // left
-            //GL.Color3(0.0, 0.0, 1.0);
-            GL.Vertex3(10.0, 10.0, 10.0);
-            GL.Vertex3(10.0, -10.0, 10.0);
-            GL.Vertex3(-10.0, -10.0, 10.0);
-            GL.Vertex3(-10.0, 10.0, 10.0);
-
-            GL.End();
 
             window.SwapBuffers();
         }
@@ -113,6 +67,7 @@ namespace ViewModel
         private void Loaded(object sender, EventArgs e)
         {
             GL.ClearColor(0f, 0f, 0f, 0f);
+            GL.Enable(EnableCap.DepthTest);
 
         }
     }
