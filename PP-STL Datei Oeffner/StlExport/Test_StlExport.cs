1435e05e772ae08da27ec9a760479dc7fc5b29b5
Michael Reno <s0565907@htw-berlin.de>
2019-05-29 10:28:38 +0200

    ...

--- PP-STL Datei Oeffner/StlExport/Test_StlExport.cs
+++ PP-STL Datei Oeffner/StlExport/Test_StlExport.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using DataModel;
 
 namespace StlExport
 {
     public class Test_StlExport
     {
+        // Collect all coordinates from PointList
+
+        // Collect all point-normal from Normal
+
+        // Compile as one STL File
     }
 }
