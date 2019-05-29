0ab55f4c8e0788b7a3cd7c89c0bb4d781ae61b99
Michael Reno <s0565907@htw-berlin.de>
2019-05-29 11:39:17 +0200

    added basic functionality of stl export

--- PP-STL Datei Oeffner/StlExport/Test_StlExport.cs
+++ PP-STL Datei Oeffner/StlExport/Test_StlExport.cs
@@ -6,14 +6,37 @@ using System.Threading.Tasks;
 using System.IO;
 using DataModel;
 
-namespace StlExport
+namespace StlExport: IEnumerable
 {
     public class Test_StlExport
     {
         // Collect all coordinates from PointList
+        DataModel.DataModel dm = new DataModel.DataModel();
+        List<Point> ListOfPoints;
 
+        //public void listCoordinates()
+        //{
+        //    foreach (Point p in dm.points.AddOrGetPoint(p))
+        //    {
+        //        ListOfPoints.Add(p);
+        //    }
+
+        //}
         // Collect all point-normal from Normal
+        List<Normal> ListOfNormals;
+
+        public void listNormal()
+        {
+            foreach (Point pts in ListOfPoints)
+            {
+                ListOfNormals.Add(Normal.FromPoint(pts));
+            }
+        }
 
         // Compile as one STL File
+        public void AsFile(List<Point> pts, List<Normal> norms)
+        {
+            File.
+        }
     }
 }
