24a732643b6344c82ea75a7cd3c240fde9559723
Maximilian <s0564319@htw-berlin.de>
2019-05-31 23:25:14 +0200

    usage of the datamodel

--- PP-STL Datei Oeffner/STLDataHandlingTool/ErrorHandlingNew/Program.cs
+++ PP-STL Datei Oeffner/STLDataHandlingTool/ErrorHandlingNew/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,13 @@ namespace ErrorHandling
     {
         static void Main(string[] args)
         {
+            PointList bla = new PointList();
+            Point punkt1 = new Point(1,2,3);
+            bla.AddOrGetPoint(punkt1);
+            Point punkt2 = new Point();
+            punkt2 = bla.GetPoint(1);
+            Console.WriteLine();
+            Console.ReadLine();
         }
     }
 }
