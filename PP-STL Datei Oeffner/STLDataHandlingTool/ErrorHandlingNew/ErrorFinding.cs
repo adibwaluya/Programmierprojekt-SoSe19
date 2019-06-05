9d3a3ba102137767d7d36c3a4e980c6a127c0956
Maximilian <s0564319@htw-berlin.de>
2019-06-05 22:27:46 +0200

    implemented the core of the function to find errors

--- PP-STL Datei Oeffner/STLDataHandlingTool/ErrorHandlingNew/ErrorFinding.cs
+++ PP-STL Datei Oeffner/STLDataHandlingTool/ErrorHandlingNew/ErrorFinding.cs
@@ -12,11 +12,11 @@ namespace ErrorHandling
     {
         //Wie kann ich hier auf die Instanz vom Datamodel zugreifen, die in der Klasse ErrorHandling erzeugt wird?
 
-        Face face1 = dm.faces.GetFace(0);
-        List<Edge> listOfEdges = new List<Edge>();
-        listOfEdges = face1.Edges;
-        Edge edge1 = listOfEdges[0];
-        Console.WriteLine(edge1.P1.Z);
+        //Face face1 = dm.faces.GetFace(0);
+        //List<Edge> listOfEdges = new List<Edge>();
+        //listOfEdges = face1.Edges;
+        //Edge edge1 = listOfEdges[0];
+        //Console.WriteLine(edge1.P1.Z);
 
 
 
