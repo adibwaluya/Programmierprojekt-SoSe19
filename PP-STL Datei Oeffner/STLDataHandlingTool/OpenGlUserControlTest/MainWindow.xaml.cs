f52c8edb8ed34f2a85a5b99b802ba074ff709e56
Michael Kaip <michael.kaip@student.htw-berlin.de>
2019-06-05 15:00:04 +0200

    fixed an accidently killed file

--- PP-STL Datei Oeffner/STLDataHandlingTool/OpenGlUserControlTest/MainWindow.xaml.cs
+++ PP-STL Datei Oeffner/STLDataHandlingTool/OpenGlUserControlTest/MainWindow.xaml.cs
@@ -1,19 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using DataModel;
 
-namespace OpenGlUserControlTest
+
+namespace ViewModelTest
 {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
@@ -23,6 +12,108 @@ namespace OpenGlUserControlTest
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // EventHandler for Btn in MAinWindow
+        private void CreateAndShowModel_Click(object sender, RoutedEventArgs e)
+        {
+            var dm = new DataModel.DataModel();
+            // Face 1
+            dm.AddPoint(1.000000e+01, -1.000000e+01, -1.000000e+01);
+            dm.AddPoint(1.000000e+01, -1.000000e+01, 1.000000e+01);
+            dm.AddPoint(-1.000000e+01, -1.000000e+01, -1.000000e+01);
+            dm.AddEdge(0, 1);
+            dm.AddEdge(0, 2);
+            dm.AddEdge(1, 2);
+            dm.AddFace(0, 1, 2, new Normal(0.000000e+00, -1.000000e+00, 0.000000e+00));
+            // Face 2
+            dm.AddPoint(-1.000000e+01, -1.000000e+01, -1.000000e+01);
+            dm.AddPoint(1.000000e+01, -1.000000e+01, 1.000000e+01);
+            dm.AddPoint(-1.000000e+01, -1.000000e+01, 1.000000e+01);
+            dm.AddEdge(3, 4);
+            dm.AddEdge(3, 5);
+            dm.AddEdge(4, 5);
+            dm.AddFace(3, 4, 5, new Normal(0.000000e+00, -1.000000e+00, 0.000000e+00));
+            // Face 3
+            dm.AddPoint(1.000000e+01, 1.000000e+01, -1.000000e+01);
+            dm.AddPoint(1.000000e+01, 1.000000e+01, 1.000000e+01);
+            dm.AddPoint(1.000000e+01, -1.000000e+01, -1.000000e+01);
+            dm.AddEdge(6, 7);
+            dm.AddEdge(6, 8);
+            dm.AddEdge(7, 8);
+            dm.AddFace(6, 7, 8, new Normal(1.000000e+00, 0.000000e+00, -0.000000e+00));
+            // Face 4
+            dm.AddPoint(1.000000e+01, -1.000000e+01, -1.000000e+01);
+            dm.AddPoint(1.000000e+01, 1.000000e+01, 1.000000e+01);
+            dm.AddPoint(1.000000e+01, -1.000000e+01, 1.000000e+01);
+            dm.AddEdge(9, 10);
+            dm.AddEdge(9, 11);
+            dm.AddEdge(10, 11);
+            dm.AddFace(9, 10, 11, new Normal(1.000000e+00, 0.000000e+00, 0.000000e+00));
+            // Face 5
+            dm.AddPoint(1.000000e+01, 1.000000e+01, 1.000000e+01);
+            dm.AddPoint(-1.000000e+01, 1.000000e+01, 1.000000e+01);
+            dm.AddPoint(1.000000e+01, -1.000000e+01, 1.000000e+01);
+            dm.AddEdge(12, 13);
+            dm.AddEdge(12, 14);
+            dm.AddEdge(13, 14);
+            dm.AddFace(12, 13, 14, new Normal(0.000000e+00, 0.000000e+00, 1.000000e+00));
+            // Face 6
+            dm.AddPoint(1.000000e+01, -1.000000e+01, 1.000000e+01);
+            dm.AddPoint(-1.000000e+01, 1.000000e+01, 1.000000e+01);
+            dm.AddPoint(-1.000000e+01, -1.000000e+01, 1.000000e+01);
+            dm.AddEdge(15, 16);
+            dm.AddEdge(15, 17);
+            dm.AddEdge(16, 17);
+            dm.AddFace(15, 16, 17, new Normal(0.000000e+00, 0.000000e+00, 1.000000e+00));
+            // Face 7
+            dm.AddPoint(-1.000000e+01, 1.000000e+01, 1.000000e+01);
+            dm.AddPoint(-1.000000e+01, 1.000000e+01, -1.000000e+01);
+            dm.AddPoint(-1.000000e+01, -1.000000e+01, 1.000000e+01);
+            dm.AddEdge(18, 19);
+            dm.AddEdge(18, 20);
+            dm.AddEdge(19, 20);
+            dm.AddFace(18, 19, 20, new Normal(-1.000000e+00, -0.000000e+00, -0.000000e+00));
+            // Face 8
+            dm.AddPoint(-1.000000e+01, -1.000000e+01, 1.000000e+01);
+            dm.AddPoint(-1.000000e+01, 1.000000e+01, -1.000000e+01);
+            dm.AddPoint(-1.000000e+01, -1.000000e+01, -1.000000e+01);
+            dm.AddEdge(21, 22);
+            dm.AddEdge(21, 23);
+            dm.AddEdge(22, 23);
+            dm.AddFace(21, 22, 23, new Normal(-1.000000e+00, 0.000000e+00, 0.000000e+00));
+            // Face 9
+            dm.AddPoint(-1.000000e+01, 1.000000e+01, -1.000000e+01);
+            dm.AddPoint(1.000000e+01, 1.000000e+01, -1.000000e+01);
+            dm.AddPoint(-1.000000e+01, -1.000000e+01, -1.000000e+01);
+            dm.AddEdge(24, 25);
+            dm.AddEdge(24, 26);
+            dm.AddEdge(25, 26);
+            dm.AddFace(24, 25, 26, new Normal(0.000000e+00, 0.000000e+00, -1.000000e+00));
+            // Face 10
+            dm.AddPoint(-1.000000e+01, -1.000000e+01, -1.000000e+01);
+            dm.AddPoint(1.000000e+01, 1.000000e+01, -1.000000e+01);
+            dm.AddPoint(1.000000e+01, -1.000000e+01, -1.000000e+01);
+            dm.AddEdge(27, 28);
+            dm.AddEdge(27, 29);
+            dm.AddEdge(28, 29);
+            dm.AddFace(27, 28, 29, new Normal(0.000000e+00, 0.000000e+00, -1.000000e+00));
+            // Face 11
+            dm.AddPoint(-1.000000e+01, 1.000000e+01, -1.000000e+01);
+            dm.AddPoint(-1.000000e+01, 1.000000e+01, 1.000000e+01);
+            dm.AddPoint(1.000000e+01, 1.000000e+01, -1.000000e+01);
+            dm.AddEdge(30, 31);
+            dm.AddEdge(30, 32);
+            dm.AddEdge(31, 32);
+            dm.AddFace(30, 31, 32, new Normal(0.000000e+00, 1.000000e+00, 0.000000e+00));
+            // Face 12
+            dm.AddPoint(1.000000e+01, 1.000000e+01, -1.000000e+01);
+            dm.AddPoint(-1.000000e+01, 1.000000e+01, 1.000000e+01);
+            dm.AddPoint(1.000000e+01, 1.000000e+01, 1.000000e+01);
+            dm.AddEdge(33, 34);
+            dm.AddEdge(33, 35);
+            dm.AddEdge(34, 35);
+            dm.AddFace(33, 34, 35, new Normal(0.000000e+00, 1.000000e+00, -0.000000e+00));
         }
     }
 }
