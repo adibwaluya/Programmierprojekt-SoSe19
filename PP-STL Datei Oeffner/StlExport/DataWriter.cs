eccf245cd09e53de218940f7264df823d5b0ad85
s0565907 <s0565907@htw-berlin.de>
2019-06-19 23:40:19 +0200

    moved test data model for STL Export to View and set View as test environment for STL Export

--- PP-STL Datei Oeffner/StlExport/DataWriter.cs
+++ PP-STL Datei Oeffner/StlExport/DataWriter.cs
@@ -10,34 +10,15 @@ using System.Globalization;
 using System.Net;
 using System.Net.Http;
 using System.Threading;
+using View;
 
 namespace StlExport
 {
     public class DataWriter
     {
-        // Collect all coordinates from PointList
+        // Collect all coordinates from PointList in test environment (from View)
         DataModel.DataStructure dm = new DataModel.DataStructure();
-        private List<Point> ListOfPoints;
-
-        private void ListCoordinates()
-        {
-            foreach (Point p in dm.points)
-            {
-                ListOfPoints.Add(p);
-            }
-        }
-
-        // Collect all point-normal from Normal
-        private List<Normal> ListOfNormals;
-
-
-        private void ListNormal()
-        {
-            foreach (Point pts in ListOfPoints)
-            {
-                ListOfNormals.Add(Normal.FromPoint(pts));
-            }
-        }
+        View.ExportTest_DataModel testDM = new ExportTest_DataModel();
 
         // Indentation as strings
         readonly string indent = String.Join("    ", new String[4]);
@@ -62,24 +43,24 @@ namespace StlExport
                 Thread.CurrentThread.CurrentCulture = current;
                 Thread.CurrentThread.CurrentUICulture = current;
 
-                for (int i = 0; i < ListOfPoints.Count; i = i + 3)
+                for (int i = 0; i < testDM.ListOfPoints.Count; i = i + 3)
                 {
                     //All normal and points as e-sign exponent format
-                    string nXasE = ListOfNormals[i].X.ToString("E");
-                    string nYasE = ListOfNormals[i].Y.ToString("E");
-                    string nZasE = ListOfNormals[i].Z.ToString("E");
+                    string nXasE = testDM.ListOfNormals[i].X.ToString("E");
+                    string nYasE = testDM.ListOfNormals[i].Y.ToString("E");
+                    string nZasE = testDM.ListOfNormals[i].Z.ToString("E");
 
-                    string iXasE = ListOfPoints[i].X.ToString("E"); // for i
-                    string iYasE = ListOfPoints[i].Y.ToString("E");
-                    string iZasE = ListOfPoints[i].Z.ToString("E");
+                    string iXasE = testDM.ListOfPoints[i].X.ToString("E"); // for i
+                    string iYasE = testDM.ListOfPoints[i].Y.ToString("E");
+                    string iZasE = testDM.ListOfPoints[i].Z.ToString("E");
 
-                    string i1XasE = ListOfPoints[i + 1].X.ToString("E"); // for i + 1
-                    string i1YasE = ListOfPoints[i + 1].Y.ToString("E");
-                    string i1ZasE = ListOfPoints[i + 1].Z.ToString("E");
+                    string i1XasE = testDM.ListOfPoints[i + 1].X.ToString("E"); // for i + 1
+                    string i1YasE = testDM.ListOfPoints[i + 1].Y.ToString("E");
+                    string i1ZasE = testDM.ListOfPoints[i + 1].Z.ToString("E");
 
-                    string i2XasE = ListOfPoints[i + 2].X.ToString("E"); // for i + 2
-                    string i2YasE = ListOfPoints[i + 2].Y.ToString("E");
-                    string i2ZasE = ListOfPoints[i + 2].Z.ToString("E");
+                    string i2XasE = testDM.ListOfPoints[i + 2].X.ToString("E"); // for i + 2
+                    string i2YasE = testDM.ListOfPoints[i + 2].Y.ToString("E");
+                    string i2ZasE = testDM.ListOfPoints[i + 2].Z.ToString("E");
 
                     //Write the body of ASCII STL Data
                     txtWriter.WriteLine($"facet normal {nXasE} {nYasE} {nZasE}");
@@ -118,7 +99,7 @@ namespace StlExport
                 txtWriter.Write(Header);
 
                 // UINT32 – Number of triangles
-                txtWriter.Write((ListOfPoints.Count / 3)); // A triangle consists of 3 points
+                txtWriter.Write((testDM.ListOfPoints.Count / 3)); // A triangle consists of 3 points
 
                 // foreach triangle
                 // REAL32[3] – Normal vector
@@ -153,29 +134,5 @@ namespace StlExport
         }
 
 
-    }
-
-    public class ExportTest_DataModel
-    {
-        public void FillDatamodel(DataStructure dm)
-        {
-            dm.AddPoint(11, 22, 33);
-            dm.AddPoint(20, 10, 30);
-            dm.AddPoint(1, 2, 3);
-            dm.AddPoint(4, 6, 8);
-
-            dm.AddEdge(0, 1);
-            dm.AddEdge(1, 2);
-            dm.AddEdge(2, 3);
-            dm.AddEdge(3, 0);
-            dm.AddEdge(3, 1);
-
-            Normal norm1 = new Normal(1, 1, 1);
-            Normal norm2 = new Normal(1, 2, 3);
-
-
-            dm.AddFace(3, 1, 2, norm1);
-            dm.AddFace(0, 1, 2, norm2);
-        }
     }
 }
