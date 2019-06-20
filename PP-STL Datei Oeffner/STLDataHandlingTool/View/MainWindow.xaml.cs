eaa14337a4fa74cc211b3bf56c5e06e0d5e5c64d
s0565907 <s0565907@htw-berlin.de>
2019-06-20 10:42:32 +0200

    added SaveFileDialog

--- PP-STL Datei Oeffner/STLDataHandlingTool/View/MainWindow.xaml.cs
+++ PP-STL Datei Oeffner/STLDataHandlingTool/View/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using DataModel;
+using Microsoft.Win32;
 using Point = DataModel.Point;
 
 
@@ -32,6 +33,27 @@ namespace View
         //{
         //    TextBox.Text = "#" + ClrPcker_Background.SelectedColor.R.ToString() + ClrPcker_Background.SelectedColor.G.ToString() + ClrPcker_Background.SelectedColor.B.ToString();
         //}
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Set the button as a save file dialog when clicked
+            SaveFileDialog SaveDlg = new SaveFileDialog();
+
+            // Set the filter so the user will know which format of stl will be saved
+            SaveDlg.Filter = "ASCII STL File (*.stl) | *.stl | Binary STL File (*.stl) | *.stl";
+            // Set the initial directory of the saved file to My Documents
+            SaveDlg.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            // Requirement to save the file
+            if (SaveDlg.ShowDialog() == true & SaveDlg.Filter == "ASCII STL File")
+            {
+                // AsAsciiFile here with SaveDlg.Filename and data model as parameter
+            }
+
+            if (SaveDlg.ShowDialog() == true & SaveDlg.Filter == "Binary STL File")
+            {
+                // AsBinaryFile here with SaveDlg.Filename and data model as parameter
+            }
+        }
     }
 
     public class ExportTest_DataModel
