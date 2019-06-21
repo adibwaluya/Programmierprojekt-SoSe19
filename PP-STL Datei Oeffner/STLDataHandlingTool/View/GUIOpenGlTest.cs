7943f6751e055d979b86d4047413ace00b81a6ab
s0565907 <s0565907@htw-berlin.de>
2019-06-21 21:17:57 +0200

    changed to user control

---
+++ PP-STL Datei Oeffner/STLDataHandlingTool/View/GUIOpenGlTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace View
+{
+    public partial class GUIOpenGlTest: UserControl
+    {
+        public GUIOpenGlTest()
+        {
+            InitializeComponent();
+        }
+
+        private void GUIOpenGlTest_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+}
