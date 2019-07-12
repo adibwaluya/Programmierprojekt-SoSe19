a8e3836595f5098005d9a17d1c9f00609c80b0f1
Michael Reno <s0565907@htw-berlin.de>
2019-07-13 00:58:37 +0200

    .

--- STLDataHandlingTool/DataModel/UserSettings.cs
+++ STLDataHandlingTool/DataModel/UserSettings.cs
@@ -9,9 +9,21 @@ namespace DataModel
 {
     public class UserSettings
     {
-        public Color BackgroundColor { get; set; }
-        public Color ForegroundColor { get; set; }
-        public Color ErrorColor { get; set; }
+        public Color BackgroundColor
+        {
+            get { return BackgroundColor; }
+            set { BackgroundColor = Color.FromRgb(10, 10, 10); }
+        }
+        public Color ForegroundColor
+        {
+            get { return ForegroundColor; }
+            set { ForegroundColor = Color.FromRgb(255, 255, 255); }
+        }
+        public Color ErrorColor
+        {
+            get { return ErrorColor; }
+            set { ErrorColor = Color.FromRgb(255, 0, 0); }
+        }
 
         public UserSettings(Color backgroundColor, Color foregroundColor, Color errorColor)
         {
