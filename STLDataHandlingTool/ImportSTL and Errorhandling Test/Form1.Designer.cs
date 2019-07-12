1b0bed0a200b9909b1f7e4f1b884055fa00ed242
Maximilian <s0564319@htw-berlin.de>
2019-07-12 20:56:13 +0200

    asdf

---
+++ STLDataHandlingTool/ImportSTL and Errorhandling Test/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace importSTLTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stlSelectBt = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // stlSelectBt
+            // 
+            this.stlSelectBt.Location = new System.Drawing.Point(118, 12);
+            this.stlSelectBt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.stlSelectBt.Name = "stlSelectBt";
+            this.stlSelectBt.Size = new System.Drawing.Size(76, 23);
+            this.stlSelectBt.TabIndex = 0;
+            this.stlSelectBt.Text = "Import STL Data";
+            this.stlSelectBt.UseVisualStyleBackColor = true;
+            this.stlSelectBt.Click += new System.EventHandler(this.stlSelectBt_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 599);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Find Errors";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 623);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.stlSelectBt);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button stlSelectBt;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ STLDataHandlingTool/ImportSTL and Errorhandling Test/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ErrorHandling;
+
+namespace importSTLTest
+{
+    public partial class Form1 : Form
+    {
+        DataModel.DataStructure dm = new DataModel.DataStructure();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void stlSelectBt_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog
+            {
+                Title = "Browse STL Data",
+                CheckFileExists = true,
+                CheckPathExists = true,
+                Filter = "STL Files|*.stl;*.txt;"
+            };
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                importSTL.DataReader read = new importSTL.DataReader(openFile.FileName);
+                dm = read.ReadFile();
+
+                textBox1.Text = Convert.ToString("import finished");
+                
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ErrorFinding errorFinding = new ErrorFinding();
+            errorFinding.FindError(dm, new System.Drawing.Color());
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; dm.edges.GetEdge(i) != null; i++)
+            {
+                if (dm.edges.GetEdge(i).CurrentCondition != DataModel.Edge.Condition.NotFaulty)
+                {
+                    sb.AppendLine("ID " + Convert.ToString(i) + " " + Convert.ToString(dm.edges.GetEdge(i).CurrentCondition));
+                }
+            }
+            sb.AppendLine("Edges not Listed here are not faulty");
+            textBox1.Text = Convert.ToString(sb);
+        }
+    }
+}
