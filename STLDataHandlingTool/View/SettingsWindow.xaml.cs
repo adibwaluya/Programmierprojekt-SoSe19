326c4f3f8fa6647223c99dcd7a151d0c9bd5539f
Michael Reno <s0565907@htw-berlin.de>
2019-07-13 00:27:44 +0200

    .

--- STLDataHandlingTool/View/SettingsWindow.xaml.cs
+++ STLDataHandlingTool/View/SettingsWindow.xaml.cs
@@ -2,22 +2,8 @@
  * Copyright (c) <2019><Michael Kaip, Maximilian Mews, Michael Reno, Adib Ghassani Waluya> *
  *******************************************************************************************/
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-//using System.Drawing;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using Xceed.Wpf;
-using View;
 using DataModel;
 
 namespace View
